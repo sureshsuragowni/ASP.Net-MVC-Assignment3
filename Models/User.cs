9cd83f5a910f63ce7eb56165b74d2d190b4130e4
Suragowni <suresh-gowd.suragowni@capgemini.com>
2022-05-02 09:08:19 +0530

    commited

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Mvc;
+using MVC_Assign.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVC_Assign.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+
+}
